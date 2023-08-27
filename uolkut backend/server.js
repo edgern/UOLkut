8f8925b1be148c769b01e160d862bdf332805879
Edgard Ernesto <edg.ernesto@gmail.com>
2023-08-27 14:27:53 -0300

    adiçao do backend do projeto / uolkut 1.0

---
+++ uolkut backend/server.js
@@ -0,0 +1,20 @@
+const jsonServer = require('json-server')
+const auth = require('json-server-auth')
+
+const app = jsonServer.create()
+const router = jsonServer.router('db.json')
+const middlewares = jsonServer.defaults()
+
+const rules = auth.rewriter({
+    users: 640
+  })
+
+app.db = router.db
+
+app.use(middlewares)
+app.use(rules)
+app.use(auth)
+app.use(router)
+app.listen(3001, () => {
+    console.log('JSON Server is running')
+})
